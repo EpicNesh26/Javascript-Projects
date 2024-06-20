abd3f69537d2cc1bf18365420f64c4038cde723a
Nesh <soninesh2006@gmail.com>
2024-06-20 14:56:29 -0600

    Modified Clutter

--- Clear Clutter/main.js
+++ Clear Clutter/main.js
@@ -17,43 +17,54 @@
 // zip/harry.zip, zip/Rohan.zip
 // this.pdf, pdf/this.pdf 
 
-const fs = require('fs');
-const path = require('path');
 
-const directoryPath = 'C:\Coding\JS Projects\Clear Clutter\files'; // Replace with your directory path
 
-fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-        console.error('Error reading directory:', err);
-        return;
 
-    }
-})
-const fileList = files.filter(file => fs.statSync(path.join(directoryPath, file)).isFile());
 
-// Create folders for each unique file extension
-const extensions = new Set(fileList.map(file => path.extname(file).toLowerCase()));
-extensions.forEach(ext => {
-    const folderName = ext.substring(1); // Remove the leading dot (e.g., ".jpg" -> "jpg")
-    const folderPath = path.join(directoryPath, folderName);
 
-    if (!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath);
-    }
-});
 
-// Move each file into the corresponding folder
-fileList.forEach(file => {
-    const ext = path.extname(file).toLowerCase();
-    const folderName = ext.substring(1); // Remove the leading dot
-    const sourcePath = path.join(directoryPath, file);
-    const destPath = path.join(directoryPath, folderName, file);
 
-    fs.rename(sourcePath, destPath, err => {
-        if (err) {
-            console.error(`Error moving file ${file} to ${folderName} folder:`, err);
-        } else {
-            console.log(`Moved ${file} to ${folderName} folder`);
-        }
-    });
-});
+
+// ATTEMPT 1:
+
+
+// const fs = require('fs');
+// const path = require('path');
+
+// const directoryPath = 'C:\Coding\JS Projects\Clear Clutter\files'; // Replace with your directory path
+
+// fs.readdir(directoryPath, (err, files) => {
+//     if (err) {
+//         console.error('Error reading directory:', err);
+//         return;
+
+//     }
+// })
+// const fileList = files.filter(file => fs.statSync(path.join(directoryPath, file)).isFile());
+
+// // Create folders for each unique file extension
+// const extensions = new Set(fileList.map(file => path.extname(file).toLowerCase()));
+// extensions.forEach(ext => {
+//     const folderName = ext.substring(1); // Remove the leading dot (e.g., ".jpg" -> "jpg")
+//     const folderPath = path.join(directoryPath, folderName);
+
+//     if (!fs.existsSync(folderPath)) {
+//         fs.mkdirSync(folderPath);
+//     }
+// });
+
+// // Move each file into the corresponding folder
+// fileList.forEach(file => {
+//     const ext = path.extname(file).toLowerCase();
+//     const folderName = ext.substring(1); // Remove the leading dot
+//     const sourcePath = path.join(directoryPath, file);
+//     const destPath = path.join(directoryPath, folderName, file);
+
+//     fs.rename(sourcePath, destPath, err => {
+//         if (err) {
+//             console.error(`Error moving file ${file} to ${folderName} folder:`, err);
+//         } else {
+//             console.log(`Moved ${file} to ${folderName} folder`);
+//         }
+//     });
+// });
