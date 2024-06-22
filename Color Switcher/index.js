2e772a314e97020e74383247abf0a25c5173816c
Nesh <soninesh2006@gmail.com>
2024-06-22 13:20:43 -0600

    Modified Color Swticher

--- Color Switcher/index.js
+++ Color Switcher/index.js
@@ -1,11 +1,14 @@
 const colorcontainer = document.getElementById('color-change')
 const colorbutton = document.getElementById('colorbutton')
+// const invertext = document.getElementsByTagName('p')
 
-const colors = ['#007bff', '#28a745', '#dc3545', '#ffc107', '#17a2b8'];
+const colors = ['#007bff', '#28a745', '#dc3545', '#ffc107', '#17a2b8', '#6f42c1', '#e83e8c', '#fd7e14', '#343a40'];
+
 
 function changecolor(){
     const randomIndex = Math.floor(Math.random()* colors.length);
     const randomColor = colors[randomIndex];
+    // invertext.style.color.filter(invert)
     colorcontainer.style.backgroundColor = randomColor;
 
 }
