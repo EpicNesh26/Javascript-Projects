cc0a46c57dedaa703635b9ea098a28f391638c2f
Nesh <soninesh2006@gmail.com>
2024-07-27 16:35:15 -0600

    Completed Display Cards

---
+++ Display Cards/Display Cards/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+
+
+import Navbar from './components/Navbar.jsx'
+import './App.css'
+
+function App() {
+  const [cards, setCards] = useState([])
+
+  const fetchData = async () => {
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data = await a.json()
+    setCards(data)
+    console.log(data)
+  }
+
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  return (
+    <>
+    <Navbar/>
+      <div className="container">
+      {cards.map((card) => {
+        return <div key={card.id} className="card">
+          <h1>{card.title}</h1>
+          <p>{card.body}</p>
+          <span>By: UserId: {card.userId}</span>
+        </div>
+      })}
+      </div>
+    </>
+  )
+}
+
+export default App
